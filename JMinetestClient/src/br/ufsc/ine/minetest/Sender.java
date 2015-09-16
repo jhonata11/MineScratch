3ed2813fe858d6efb319365d9679117a95481bd1
Jhonata da Rocha <jhonata11@hotmail.com>
2015-09-16 03:27:10 -0300

    desfazendo modificações

--- JMinetestClient/src/br/ufsc/ine/minetest/Sender.java
+++ JMinetestClient/src/br/ufsc/ine/minetest/Sender.java
@@ -1,6 +1,5 @@
 package br.ufsc.ine.minetest;
 
-import java.net.BindException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -8,7 +7,7 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.concurrent.Semaphore;
 
-public class Sender {
+public class Sender{
 
 	private static final short SEQNUM_INITIAL = (short) 65500;
 	private short seqNum;
@@ -17,25 +16,25 @@ public class Sender {
 	private String host;
 	private int port;
 	private short acked;
-
+	
 	int count = 0;
 	private Semaphore semaphore;
+
 
 	public Sender(String host, int port, Semaphore semaphore) throws InterruptedException {
 		this.semaphore = semaphore;
 		this.seqNum = SEQNUM_INITIAL;
 		minetestProtocol = new MinetestProtocol(this);
-
+		
 		this.host = host;
 		this.port = port;
 	}
 
 	/**
 	 * Starts the handshake with the MinetestServer
-	 * 
-	 * @param username
-	 * @param password
-	 * @throws Exception
+	 * @param username 
+	 * @param password 
+	 * @throws Exception 
 	 */
 	public void startHandshake(String username, String password) throws Exception {
 		MinetestPacket packet = new MinetestPacket();
@@ -48,21 +47,21 @@ public class Sender {
 		byte[] toServerInit2 = ByteBuffer.allocate(2).putShort((short) 0x11).array();
 		MinetestPacket a = new MinetestPacket();
 		a.addToBodyStart(toServerInit2);
-
+		
 		this.sendCommand(a);
 	}
-
-	public void disconnect() throws Exception {
+	
+	public void disconnect() throws Exception{
 		MinetestPacket packet = new MinetestPacket();
 		packet.addToBodyStart(this.minetestProtocol.disconnect());
 		this.send(packet);
 	}
-
-	public void startReliableConnection() throws Exception {
+	
+	public void startReliableConnection() throws Exception{
 		this.sendCommand(new MinetestPacket());
 	}
-
-	public void ack(short seqnum) throws Exception {
+	
+	public void ack(short seqnum) throws Exception{
 		MinetestPacket packet = new MinetestPacket();
 		packet.addToBodyEnd(minetestProtocol.createAckPackage(seqnum));
 		this.send(packet);
@@ -70,10 +69,8 @@ public class Sender {
 
 	/**
 	 * Sends a command to the MinetestServer with a given message
-	 * 
-	 * @param packet
-	 *            the message to be sent to the MinetestServer
-	 * @throws Exception
+	 * @param packet the message to be sent to the MinetestServer
+	 * @throws Exception 
 	 */
 	public void sendCommand(MinetestPacket packet) throws Exception {
 		packet.addToBodyStart(minetestProtocol.createCommandByte());
@@ -89,18 +86,19 @@ public class Sender {
 	private void send(MinetestPacket packet) throws Exception {
 		packet.addToHeader(minetestProtocol.createHeader());
 		byte[] sendData = packet.converToMessage();
-		sendDataToServer(sendData);
+        sendDataToServer(sendData);
 	}
 
+	
 	private void sendDataToServer(byte[] sendData) throws Exception {
 		semaphore.acquire();
-
-		InetAddress address = InetAddress.getByName(this.host);
-		DatagramPacket packet3 = new DatagramPacket(sendData, sendData.length, address, this.port);
-		DatagramSocket datagramSocket = new DatagramSocket(this.port);
-		datagramSocket.send(packet3);
-		datagramSocket.close();
-
+        InetAddress address = InetAddress.getByName(this.host);
+        DatagramPacket packet3 = new DatagramPacket(sendData, sendData.length, address, this.port);
+        DatagramSocket datagramSocket = new DatagramSocket(this.port);
+        datagramSocket.send(packet3);
+        datagramSocket.close();
+        
+//		System.out.println(count + " === SENT: " + Arrays.toString(sendData));
 		count++;
 		semaphore.release();
 	}
@@ -117,14 +115,13 @@ public class Sender {
 	}
 
 	/**
-	 * @param minetestProtocol
-	 *            the minetestProtocol to set
+	 * @param minetestProtocol the minetestProtocol to set
 	 */
 	public void setMinetestProtocol(MinetestProtocol minetestProtocol) {
 		this.minetestProtocol = minetestProtocol;
 	}
-
-	public Semaphore getSemaphore() {
+	
+	public Semaphore getSemaphore(){
 		return this.semaphore;
 	}
 }
